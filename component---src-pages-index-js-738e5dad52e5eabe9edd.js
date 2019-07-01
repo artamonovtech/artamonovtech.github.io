a332e093a5ed464036969b3cde626f821007f202
Alexandr Artamonov <artatics@yandex.ru>
2019-07-01 07:50:31 +0300

    Updates

---
+++ component---src-pages-index-js-738e5dad52e5eabe9edd.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{155:function(n,e,t){"use strict";t.r(e);var a=t(0),o=t.n(a);e.default=function(){return o.a.createElement("div",null,"Alexander Artamonov")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-738e5dad52e5eabe9edd.js.map
